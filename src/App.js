224ae96cd9d33b7eee6858126c2a23ebb5545fa7
Jake-Egbert <jake@devpipeline.com>
2023-12-21 10:45:13 -0700

    example for use callback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback } from "react";
 
-function App() {
+import useToggle from "./useToggle";
+import List from "./List";
+
+export default function App() {
+  const [number, setNumber] = useState(1);
+  const [dark, setDark] = useToggle(false);
+
+  // const getItems = () => {
+  //   return [number, number + 1, number + 2];
+  // };
+
+  // const getItems = useCallback(() => {
+  //   return [number, number + 1, number + 2];
+  // }, [number]);
+
+  const getItems = useCallback(
+    (incrementor) => {
+      return [
+        number + incrementor,
+        number + 1 + incrementor,
+        number + 2 + incrementor,
+      ];
+    },
+    [number]
+  );
+
+  const theme = {
+    backgroundColor: dark ? "#333" : "#fff",
+    color: dark ? "#fff" : "#333",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={theme}>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Toggle Theme
+      </button>
+      <List getItems={getItems} />
     </div>
   );
 }
-
-export default App;
